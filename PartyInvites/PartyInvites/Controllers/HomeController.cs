8577b0f5e7e7fd56f7a0dd8c00d27820e80cff95
stikhonov <stikhonov@hotmail.com>
2018-07-08 17:11:23 +0200

    PartyInvites - Thanks view added

--- PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -23,7 +23,8 @@ namespace PartyInvites.Controllers
         [HttpPost]
         public ViewResult RsvpForm(GuestResponse guestResponse)
         {
-            return View();
+            Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
         }
     }
 }
