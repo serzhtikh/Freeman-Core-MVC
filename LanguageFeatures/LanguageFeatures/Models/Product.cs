960020b9bd54b6c499b4e5787d628fe7989dc95d
stikhonov <stikhonov@hotmail.com>
2018-07-26 22:20:29 +0200

    LanguageFeatures - Product model added

---
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+
+        public static Product[] GetProducts()
+        {
+            var kayak = new Product {Name = "Kayak", Price = 275M};
+            var lifejacket = new Product {Name = "Lifrjacket", Price = 48.95M};
+
+            return new Product[] {kayak, lifejacket, null};
+        }
+    }
+}
